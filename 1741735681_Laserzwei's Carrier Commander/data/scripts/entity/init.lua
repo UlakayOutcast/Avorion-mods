ee33bdc44e45237b0225984d7e8fe226c8babe2e
Ulukay <95168076+UlakayOutcast@users.noreply.github.com>
2026-02-19 09:49:14 +0700

    Delete 1741735681_Laserzwei's Carrier Commander directory

--- 1741735681_Laserzwei's Carrier Commander/data/scripts/entity/init.lua
+++
@@ -1,9 +0,0 @@
-if onServer() then
-
-local entity = Entity()
-if entity.isShip or entity.isStation then
-    if entity.allianceOwned or entity.playerOwned then
-        entity:addScriptOnce("data/scripts/entity/CarrierCommander.lua")
-    end
-end
-end
