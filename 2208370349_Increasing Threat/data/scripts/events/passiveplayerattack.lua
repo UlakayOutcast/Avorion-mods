ce5fb5df67099664dc59ce1fed0f84d6797eb334
Ulukay <Ulukay@inbox.ru>
2026-04-14 22:16:45 +0700

    Update passiveplayerattack.lua

--- 2208370349_Increasing Threat/data/scripts/events/passiveplayerattack.lua
+++ 2208370349_Increasing Threat/data/scripts/events/passiveplayerattack.lua
@@ -143,15 +143,15 @@ function spawnPirates(entry)
 
     if entry:getEntityType() == EntityType.Station then
         if _LargeAttack then
-            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "На вашу станцию в секторе \\s (% 1%:%2%) совершен налет!"%_T, sector:getCoordinates())
+            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "На вашу станцию в секторе \\s(% 1%:%2%) совершен налет!"%_T, sector:getCoordinates())
         else
-            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваша станция в секторе \\s (% 1%:% 2%) атакована!"%_T, sector:getCoordinates())
+            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваша станция в секторе \\s(% 1%:% 2%) атакована!"%_T, sector:getCoordinates())
         end
     else
         if _LargeAttack then
-            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваш корабль в секторе \\s (% 1%:% 2%) захвачен!"%_T, sector:getCoordinates())
+            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваш корабль в секторе \\s(% 1%:% 2%) на прицеле!"%_T, sector:getCoordinates())
         else
-            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваш корабль в секторе \\s (% 1%:%2%) атакован!"%_T, sector:getCoordinates())
+            _Player:sendChatMessage(entry.name, ChatMessageType.Warning, "Ваш корабль в секторе \\s(% 1%:%2%) атакован!"%_T, sector:getCoordinates())
         end
     end
 end
